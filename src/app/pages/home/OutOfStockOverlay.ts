73f9cc16f25a44221da1a715b658c8cf72fad272
joysk <skrullweb.dev@gmail.com>
2020-05-15 18:52:52 +1000

    Pixi graphics

--- src/app/pages/home/OutOfStockOverlay.ts
+++ src/app/pages/home/OutOfStockOverlay.ts
@@ -44,18 +44,18 @@
       LocalizationService.instance.registerPixiTextObject(this.OUT_OF_STOCK_KEY, this._preTitle, this.objectId);
       LocalizationService.instance.registerPixiTextObject(this.OUT_OF_STOCK_SUBTITLE_KEY, this._title, this.objectId);
 
-      // this._preTitle.anchor.set(0.5, 0.5);
-      // this._preTitle.y = -this._radius * 0.3;
-      // this._title.anchor.set(0.5, 0.5);
+      this._preTitle.anchor.set(0.5, 0.5);
+      this._preTitle.y = -this._radius * 0.3;
+      this._title.anchor.set(0.5, 0.5);
 
-      // this._overlayCircle = new PIXI.Graphics();
-      // this._overlayCircle.beginFill(0xf808080, this._transparency);
-      // this._overlayCircle.drawCircle(0, 0, this._radius);
-      // this._overlayCircle.endFill();
-      // this._container.addChild(this._overlayCircle);
+      this._overlayCircle = new PIXI.Graphics();
+      this._overlayCircle.beginFill(0xf808080, this._transparency);
+      this._overlayCircle.drawCircle(0, 0, this._radius);
+      this._overlayCircle.endFill();
+      this._container.addChild(this._overlayCircle);
 
-      // this._container.addChild(this._title);
-      // this._container.addChild(this._preTitle);
+      this._container.addChild(this._title);
+      this._container.addChild(this._preTitle);
     }
 
     public destroy() {
